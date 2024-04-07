68eb068611af10a2950334252978955b03995473
Nasser Amini <namini40@gmail.com>
2024-04-07 18:54:49 +0330

    add migration using unique_id from older versions of sdk (lower than 2.x.y)

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,68 @@
-import { useEffect } from 'react';
-import { _adtraceEvent, _adtraceGetAttribution, _adtraceGetUUID, _initAdtraceChannel } from './utils/adtrace'
+import {useEffect} from 'react';
+import {
+    _adtraceEvent,
+    _adtraceGetAttribution,
+    _adtraceGetUUID,
+    _initAdtraceChannel,
+     setupMockIndexedDB
+} from './utils/adtrace'
 
 
 export default function Home() {
+
+    // IMPORTANT: run this only once before Adtrace init!
+    function mockDB(unique_id_from_older_versions) {
+        setupMockIndexedDB(unique_id_from_older_versions)
+            .then(() => {
+                console.log('IndexedDB setup successful');
+            })
+            .catch((error) => {
+                console.error('Error setting up IndexedDB:', error);
+            });
+    }
+
+
     useEffect(() => {
         _initAdtraceChannel();
     }, [])
-    
-  
-  return (
-      <main>
-          <h3>Adtrace Web SDK Demo (React.js) with power of vite</h3>
-          
-          <button
-              
-              onClick={() => _adtraceEvent({
-                  eventToken: 'qpw44u',
-                  revenue: 10,
-                  currency: 'IRR',
-                  callbackParams: [
-                      {key: 'key', value: 'value'},
-                      {key: 'foo', value: 'bar'}
-                  ]
-              })}
-          >
-              Event tracking
-          </button>
-          
-          <button
-            
-              onClick={() => {
-                  console.log(_adtraceGetAttribution())
-              }}
-          >
-              GetAttribution (console)
-          </button>
 
-          <button
-              
-              onClick={() => {
-                  console.log(_adtraceGetUUID())
-              }}
-          >
-              GetWebUUID (console)
-          </button>
-      </main>
-  )
+
+    return (
+        <main>
+            <h3>Adtrace Web SDK Demo (React.js) with power of vite</h3>
+
+            <button
+
+                onClick={() => _adtraceEvent({
+                    eventToken: 'qpw44u',
+                    revenue: 10,
+                    currency: 'IRR',
+                    callbackParams: [
+                        {key: 'key', value: 'value'},
+                        {key: 'foo', value: 'bar'}
+                    ]
+                })}
+            >
+                Event tracking
+            </button>
+
+            <button
+
+                onClick={() => {
+                    console.log(_adtraceGetAttribution())
+                }}
+            >
+                GetAttribution (console)
+            </button>
+
+            <button
+
+                onClick={() => {
+                    console.log(_adtraceGetUUID())
+                }}
+            >
+                GetWebUUID (console)
+            </button>
+        </main>
+    )
 }
--- src/utils/adtrace.jsx
+++ src/utils/adtrace.jsx
@@ -2,61 +2,108 @@ let Adtrace = null
 
 //Adtrace
 export async function initAdtrace() {
-	if (!Adtrace) {
-		return new Promise((resolve) => {
-			import('web-adtrace').then((res) => {
-				Adtrace = res.default
-				resolve(Adtrace)
-			})
-		})
-	} else {
-		return new Promise((resolve) => {
-			resolve(Adtrace)
-		})
-	}
+    if (!Adtrace) {
+        return new Promise((resolve) => {
+            import('web-adtrace').then((res) => {
+                Adtrace = res.default
+                resolve(Adtrace)
+            })
+        })
+    } else {
+        return new Promise((resolve) => {
+            resolve(Adtrace)
+        })
+    }
 }
 
 //init Adtrace
 export async function _initAdtraceChannel() {
-	await initAdtrace()
-	Adtrace.initSdk({
-		appToken: 'igtga9fy3nhe',
-		environment: 'production', //sandbox production
-	})
+    await initAdtrace()
+    if (!localStorage.getItem('token')) {
 
-	setTimeout(() => {
-		const webUUID = Adtrace.getWebUUID()
-		console.log('adtrace webUUID:' + webUUID)
-	}, 1000)
+    }
+    Adtrace.initSdk({
+        appToken: 'cf0mz487gkdt',
+        environment: 'sandbox', //sandbox production,
+        logLevel: 'verbose'
+    })
+
+
+    setTimeout(() => {
+        const webUUID = Adtrace.getWebUUID()
+        console.log('adtrace webUUID:' + webUUID)
+    }, 1000)
 }
 
 
 //Adtrace Event
 export async function _adtraceEvent(data) {
-	try {
-		await initAdtrace()
-		Adtrace.trackEvent(data)
-	} catch (error) {
-		console.error('adtraceEvent error', error)
-	}
+    try {
+        await initAdtrace()
+        Adtrace.trackEvent(data)
+    } catch (error) {
+        console.error('adtraceEvent error', error)
+    }
 }
 
 //Adtrace getAttribution
 export async function _adtraceGetAttribution() {
-	try {
-		await initAdtrace()
-		return Adtrace.getAttribution()
-	} catch (error) {
-		console.error('adtraceGetAttribution error', error)
-	}
+    try {
+        await initAdtrace()
+        return Adtrace.getAttribution()
+    } catch (error) {
+        console.error('adtraceGetAttribution error', error)
+    }
 }
 
 //Adtrace getUUID
 export async function _adtraceGetUUID() {
-	try {
-		await initAdtrace()
-		return Adtrace.getWebUUID()
-	} catch (error) {
-		console.error('adtraceGetUUID error', error)
-	}
+    try {
+        await initAdtrace()
+        return Adtrace.getWebUUID()
+    } catch (error) {
+        console.error('adtraceGetUUID error', error)
+    }
+}
+
+// run this only once before init
+export function setupMockIndexedDB(unique_id) {
+    return new Promise((resolve, reject) => {
+        const dbPromise = window.indexedDB.open("adtrace-sdk", 1);
+
+        dbPromise.onupgradeneeded = function (event) {
+            const db = event.target.result;
+
+            // Create empty database
+            if (!db.objectStoreNames.contains('data')) {
+                db.createObjectStore('q', {keyPath: 't'});
+                const store = db.createObjectStore('gp', {keyPath: 'kt'});
+                db.createObjectStore('as', {keyPath: 'u'});
+                db.createObjectStore('ed', {keyPath: 'ii', autoIncrement: true});
+                store.createIndex('tIndex', 't')
+            }
+        };
+
+        dbPromise.onsuccess = function (event) {
+
+            const db = event.target.result;
+            const transaction = db.transaction(['as'], 'readwrite');
+            const store = transaction.objectStore('as');
+
+            const request = store.put({"u": unique_id});
+
+
+            request.onsuccess = () => {
+                resolve();
+            };
+
+            request.onerror = (error) => {
+                reject(error);
+            };
+        };
+
+        dbPromise.onerror = function (event) {
+            reject(event.target.error);
+        };
+    });
 }
